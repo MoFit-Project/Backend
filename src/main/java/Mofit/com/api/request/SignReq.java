7ec8f457f330a03f9ef2974b1d11eb6276781836
enaa99 <wprkfdlsgk@gmail.com>
2023-02-23 22:46:09 +0900

    mofit_backend server

---
+++ src/main/java/Mofit/com/api/request/SignReq.java
@@ -0,0 +1,18 @@
+package Mofit.com.api.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignReq {
+    private Long id;
+
+    private String account;
+
+    private String password;
+
+    private String nickname;
+
+
+}
---
+++ src/main/java/Mofit/com/api/response/SignRes.java
@@ -0,0 +1,39 @@
+package Mofit.com.api.response;
+
+import Mofit.com.Domain.Authority;
+import Mofit.com.Domain.Member;
+import Mofit.com.security.TokenDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class SignRes {
+
+    private Long id;
+
+    private String account;
+
+    private String nickname;
+
+    private String email;
+
+    private List<Authority> roles;
+
+    private TokenDto token;
+
+    public SignRes(Member member) {
+        this.id = member.getId();
+        this.account = member.getAccount();
+        this.nickname = member.getNickname();
+        this.roles = member.getRoles();
+    }
+}
