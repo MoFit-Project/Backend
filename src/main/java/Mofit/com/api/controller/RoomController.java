017bbd739eb2f238cc26b8cd31ca5305af1ba593
enaa99 <wprkfdlsgk@gmail.com>
2023-03-06 02:07:11 +0900

    check logs

--- src/main/java/Mofit/com/api/controller/RoomController.java
+++ src/main/java/Mofit/com/api/controller/RoomController.java
@@ -74,24 +74,26 @@ public class RoomController {
         if (session == null) {
             return "No";
         }
-//        for (RoomRes value : roomHashMap.values()) {
-//            if (value.getRoomId().equals(sessionId)) {
-//                if(value.getParticipant() > 2){
-//                    ConnectionProperties properties = new ConnectionProperties.Builder()
-//                            .type(ConnectionType.WEBRTC)
-//                            .role(OpenViduRole.SUBSCRIBER)
-//                            .data("")
-//                            .build();
-//                    Connection connection = session.createConnection(properties);
-//                    return connection.getToken();
-//                }else {
-//                    ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
-//                    Connection connection = session.createConnection(properties);
-//                    return connection.getToken();
-//                }
-//
-//            }
-//        }
+/* subscriber 추가
+        for (RoomRes value : roomHashMap.values()) {
+            if (value.getRoomId().equals(sessionId)) {
+                if(value.getParticipant() > 2){
+                    ConnectionProperties properties = new ConnectionProperties.Builder()
+                            .type(ConnectionType.WEBRTC)
+                            .role(OpenViduRole.SUBSCRIBER)
+                            .data("")
+                            .build();
+                    Connection connection = session.createConnection(properties);
+                    return connection.getToken();
+                }else {
+                    ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+                    Connection connection = session.createConnection(properties);
+                    return connection.getToken();
+                }
+
+            }
+        }
+*/
         ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
         Connection connection = session.createConnection(properties);
 
